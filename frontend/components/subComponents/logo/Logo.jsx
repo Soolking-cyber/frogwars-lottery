2aa2e3acda11372e2bbfd294f875d488c22f00b7
Soolking-cyber <54062088+Soolking-cyber@users.noreply.github.com>
2024-07-05 21:11:43 +0700

    Revert "Fix image size"

--- frontend/components/subComponents/logo/Logo.jsx
+++ frontend/components/subComponents/logo/Logo.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Logo() {
   return (
     <div className='flex items-center cursor-pointer'>
-      <Image width={"56px"} height={"56px"} src="/images/crystal.gif" alt='award image' />
+      <Image className='w-14' src="/images/crystal.gif" alt='award image' />
       <p className='px-3 text-xl text-purple-500 font-customFont'>FrogWars Lottery</p>
     </div>
   )
