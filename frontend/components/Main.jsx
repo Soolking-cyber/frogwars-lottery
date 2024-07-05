126af289c017ca0d13f2c458faa5dd94d9c6bf39
Soolking-cyber <54062088+Soolking-cyber@users.noreply.github.com>
2024-07-05 12:33:42 +0700

    Update Main.jsx

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -29,8 +29,8 @@ export default function PageContent({content}) {
         <div className={styles.collectionContainer}>
           {content}
         </div>
+        <a href="https://www.nile.build/swap?to=0x21d624c846725ABe1e1e7d662E9fB274999009Aa" type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Get Crystals</a>
       </main>
-      <a href="https://www.nile.build/swap?to=0x21d624c846725ABe1e1e7d662E9fB274999009Aa" type="button" style={{alignContent: "center"}} className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Get Crystals</a>
     </div>
   );
 }
